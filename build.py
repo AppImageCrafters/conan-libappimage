088f2a791d4d9dcace6f9b07466cc3d3951125b3
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-01-24 15:42:00 -0600

    Add missing repo

--- build.py
+++ build.py
@@ -1,7 +1,11 @@
 from cpt.packager import ConanMultiPackager
 
-
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    docker_entry_script = \
+        "conan remote add appimage https://api.bintray.com/conan/azubieta/AppImage &&" \
+        "conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan &&" \
+        "sudo apt-get -qq update && sudo apt-get -qq install -y desktop-file-utils libglib2.0-dev"
+
+    builder = ConanMultiPackager(docker_entry_script=docker_entry_script)
     builder.add_common_builds(shared_option_name="libappimage:shared")
     builder.run()
