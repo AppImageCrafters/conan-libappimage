63e454ffe899e4275dab022ac8e5d7ce865f54f9
Alexis Lopez Zubieta <contact@azubieta.net>
2019-05-03 17:23:42 -0500

    Don't link against libstdc++11 with gcc4.8

--- build.py
+++ build.py
@@ -12,8 +12,4 @@ if __name__ == "__main__":
     builder = ConanMultiPackager(build_policy="missing", remotes=remotes, docker_entry_script=docker_entry_script)
     builder.add_common_builds(shared_option_name="libappimage:shared")
 
-    # libstc++11 is required by gtest therefore by the whole build
-    for settings, options, env_vars, build_requires, reference in builder.items:
-        settings["compiler.libcxx"] = 'libstdc++11'
-
     builder.run()
